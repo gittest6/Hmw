d6d8d76412087bfd894e6c40af33c95e7be7a613
Pavel Sharapov <un@mail.unetcom.ru>
2024-10-17 13:39:21 +0300

    homework 6

---
+++ Parallel/FileTasks.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hmw.Parallel;
+
+class FileTasks
+{
+    public ConcurrentQueue<string> cqErrorMessages = new ();
+    
+    public async Task<int> CountSpacesInFile (string path, int blockSize = 10000)
+    {
+        int spaceCount = 0;
+        try
+        {
+            var buffer = new char[blockSize];
+            using var reader = new StreamReader (path);
+            while (true)
+            {
+                var readCount = await reader.ReadBlockAsync (buffer, 0, buffer.Length);
+                if (readCount == 0) break;
+                if (readCount < buffer.Length) Array.Resize (ref buffer, readCount);
+                spaceCount += buffer.Count (c => c == ' ');
+            }
+        }
+        catch (Exception ex)
+        {
+            cqErrorMessages.Enqueue (ex.Message);
+        }
+        return spaceCount;
+    }
+
+    public async Task<int> CountSpacesInDirFiles (string dirPath, bool parallel = true)
+    {
+        var enumerationOptions = new EnumerationOptions () { RecurseSubdirectories = true };
+        var collFilePaths = Directory.EnumerateFiles (dirPath, "*", enumerationOptions);
+        cqErrorMessages.Clear ();
+
+        var collTasks = collFilePaths.Select (filePath => CountSpacesInFile (filePath));
+        if (parallel)
+        {
+            var arrResults = await Task.WhenAll (collTasks);
+            return arrResults.Sum ();
+        }
+        else
+            return collTasks.Select (task => task.Result).Sum ();
+    }
+}
---
+++ Parallel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Hmw.Parallel;
+
+var dirPath = Environment.GetFolderPath (Environment.SpecialFolder.UserProfile);
+var ft = new FileTasks ();
+int spaceCount;
+
+var enumerationOptions = new EnumerationOptions () { RecurseSubdirectories = true };
+var collFilePaths = Directory.EnumerateFiles (dirPath, "*", enumerationOptions).Take (3);
+var collTasks = collFilePaths.Select (filePath => ft.CountSpacesInFile (filePath));
+var arrResults = await Task.WhenAll (collTasks);
+spaceCount = arrResults.Sum ();
+Console.WriteLine (spaceCount);
+if (!ft.cqErrorMessages.IsEmpty)
+    foreach (var msg in ft.cqErrorMessages)
+        Console.WriteLine (msg);
+
+var sw = new Stopwatch ();
+foreach (var parallel in new[] { true, false })
+{
+    sw.Restart ();
+    spaceCount = await ft.CountSpacesInDirFiles (dirPath, parallel);
+    sw.Stop ();
+    Console.WriteLine (spaceCount);
+    Console.WriteLine (sw.Elapsed);
+    if (!ft.cqErrorMessages.IsEmpty)
+        foreach (var msg in ft.cqErrorMessages)
+            Console.WriteLine (msg);
+}
